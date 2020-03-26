cbf57363777520549bbef4447584731526b51fb4
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-03-26 16:30:39 +0100

    dump

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    //Parameters
+    [SerializeField] float thrustForce;
+    [SerializeField] float rotateForce;
+    
+    //Cache
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ProcessInput(); 
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotateForce * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * -rotateForce * Time.deltaTime);
+        }
+    }
+}
