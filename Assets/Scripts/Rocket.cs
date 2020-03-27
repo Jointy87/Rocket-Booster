9a54eb8c2f3af040d6807ddf3a7d0869b68d30bf
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-03-27 13:20:49 +0100

    dump

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,37 +5,47 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    //Parameters
-    [SerializeField] float thrustForce;
-    [SerializeField] float rotateForce;
-    
-    //Cache
-    Rigidbody rb;
+	//Parameters
+	[SerializeField] float thrustForce;
+	[SerializeField] float rotateForce;
+	
+	//Cache
+	Rigidbody rb;
+	AudioSource ac;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		ac = GetComponent<AudioSource>();
+	}
 
-    void Update()
-    {
-        ProcessInput(); 
-    }
+	void Update()
+	{
+		ProcessInput();
+	}
 
-    private void ProcessInput()
-    {
-        if(Input.GetKey(KeyCode.Space))
-        {
-            rb.AddRelativeForce(Vector3.up * thrustForce);
-        }
+	private void ProcessInput()
+	{
+		if(Input.GetKey(KeyCode.Space))
+		{
+			rb.AddRelativeForce(Vector3.up * thrustForce);
+			if(!ac.isPlaying)
+			{
+				ac.Play();
+			}
+		}
+		else
+		{
+			ac.Stop();
+		}
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward * rotateForce * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.forward * -rotateForce * Time.deltaTime);
-        }
-    }
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(Vector3.forward * rotateForce * Time.deltaTime);
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(Vector3.forward * -rotateForce * Time.deltaTime);
+		}
+	}
 }
